6ba86bb78302b87a5c6cc159e6f63751b5a1b82e
Hashara <kumarasinghe.k.a.s.h@gmail.com>
2026-02-19 15:31:59 +1100

    add cuda flags

--- build-scripts/jenkins-cmake-build-nvhpc.sh
+++ build-scripts/jenkins-cmake-build-nvhpc.sh
@@ -11,20 +11,13 @@ params=$3
 #work_dir=$1 # build dir
 #code_dir=$2 # iqtree2 dir
 #
-#params=$3
-
-if [ "$params" == "CUDA" ]; then
-    echo "building nvhpc-cuda"
-    cmake_params="-DUSE_CUDA=ON"
-else
-    echo "building nvhpc-vanila"
-    cmake_params="-DUSE_CUDA=OFF"
-fi
+#params=$
 
 
 ### pre steps #####
 module load openmpi/4.1.5 boost/1.84.0 nvhpc-compilers/24.7
 
+export PATH="$NVHPC_CUDA_HOME/bin:$PATH"
 
 export OMPI_CC=nvc
 export OMPI_CXX=nvc++
@@ -33,8 +26,7 @@ export CC=nvc
 export CXX=nvc++
 
 export NVHPC_CUDA_HOME=/apps/nvidia-hpc-sdk/24.7/Linux_x86_64/24.7/cuda
-export CUDACXX="$NVHPC_CUDA_HOME/bin/nvcc"
-export PATH="$NVHPC_CUDA_HOME/bin:$PATH"
+export CUDACXX=nvcc
 
 if [ "$params" = "CUDA" ] && [ ! -x "$CUDACXX" ]; then
   echo "ERROR: CUDA compiler not found at $CUDACXX"
@@ -52,9 +44,8 @@ echo "building nvhpc-vanila"
 
 mkdir -p "$work_dir"
 cd $work_dir
-cmake -DCMAKE_CXX_FLAGS="$LDFLAGS $CPPFLAGS" -DCMAKE_CUDA_COMPILER="$CUDACXX" \
-      -DCMAKE_CUDA_HOST_COMPILER="$CXX" -DCMAKE_CUDA_RUNTIME_LIBRARY=Shared \
-      -DEIGEN3_INCLUDE_DIR=/scratch/dx61/sa0557/iqtree2/eigen-3.4.0 -DUSE_CMAPLE=OFF ${cmake_params} $code_dir
+cmake -DCMAKE_CXX_FLAGS="$LDFLAGS $CPPFLAGS" \
+      -DEIGEN3_INCLUDE_DIR=/scratch/dx61/sa0557/iqtree2/eigen-3.4.0 -DUSE_CMAPLE=OFF -DUSE_CUDA=ON $code_dir
 make -j > $work_dir/build.log 2>&1
 
 
