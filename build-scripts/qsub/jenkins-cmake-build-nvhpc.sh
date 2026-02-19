f132d29646ceed55a03e2ff829ef2b562f7f4518
Hashara <kumarasinghe.k.a.s.h@gmail.com>
2026-02-20 09:55:39 +1100

    fix qsub script

--- build-scripts/qsub/jenkins-cmake-build-nvhpc.sh
+++ build-scripts/qsub/jenkins-cmake-build-nvhpc.sh
@@ -33,14 +33,14 @@ fi
 
 
 ### pre steps #####
-module load openmpi/4.1.5 boost/1.84.0 nvhpc-compilers/24.7
-
+module load openmpi/4.1.5 boost/1.84.0 nvhpc-compilers/24.7 cuda/12.5.1
 
 export OMPI_CC=nvc
 export OMPI_CXX=nvc++
 
 export CC=nvc
 export CXX=nvc++
+
 export CUDACXX=nvcc
 
 export LDFLAGS="-L/apps/nvidia-hpc-sdk/24.7/Linux_x86_64/24.7/compilers/lib"
@@ -49,11 +49,26 @@ export CPPFLAGS="-I/apps/nvidia-hpc-sdk/24.7/Linux_x86_64/24.7/compilers/include
 
 ############
 
-echo "building nvhpc-vanila"
+
+echo "building nvhpc-cuda"
 
 mkdir -p "$work_dir"
 cd $work_dir
-cmake -DCMAKE_CXX_FLAGS="$LDFLAGS $CPPFLAGS" -DEIGEN3_INCLUDE_DIR=/scratch/dx61/sa0557/iqtree2/eigen-3.4.0 -DUSE_CMAPLE=OFF ${cmake_params} $code_dir
+#cmake -DCMAKE_CXX_FLAGS="$LDFLAGS $CPPFLAGS" -DEIGEN3_INCLUDE_DIR=/scratch/dx61/sa0557/iqtree2/eigen-3.4.0 -DUSE_CMAPLE=OFF -DUSE_CUDA=ON $code_dir
+cmake -S "$code_dir" -B "$work_dir" \
+  -DCMAKE_C_COMPILER=nvc \
+  -DCMAKE_CXX_COMPILER=nvc++ \
+  -DCMAKE_CUDA_COMPILER="$(command -v nvcc)" \
+  -DCMAKE_POLICY_DEFAULT_CMP0074=NEW \
+  -DCUDAToolkit_ROOT="$(dirname "$(dirname "$(command -v nvcc)")")" \
+  -DEIGEN3_INCLUDE_DIR=/scratch/dx61/sa0557/iqtree2/eigen-3.4.0 \
+  -DUSE_CMAPLE=OFF \
+  -DUSE_CUDA=ON > $work_dir/compiler.log 2>&1
+
 make -j > $work_dir/build.log 2>&1
+
+
+
+
 
 
